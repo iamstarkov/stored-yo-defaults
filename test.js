e308b039183c338131df4f6cd1e127671069c9b8
Vladimir Starkov <iamstarkov@gmail.com>
2016-03-07 17:50:38 +0100

    write tests

--- test.js
+++ test.js
@@ -1,8 +1,38 @@
 import test from 'ava';
 import storedYoDefaults from './index';
 
+const input = [{
+  name: 'moduleVersion',
+  message: '☯ preferred version to start:',
+  store: true,
+  default: '0.0.0',
+}, {
+  name: 'moduleLicense',
+  message: '☯ preferred license:',
+  store: true,
+  default: 'MIT',
+}, {
+  name: 'moduleTest',
+  message: '☯ preferred test framework:',
+  type: 'list',
+  choices: ['mocha', 'tape', 'ava'],
+  store: true,
+  default: 1,
+}, {
+  name: 'username',
+  default: 'asd',
+}, {
+  name: 'website',
+  message: '☯ your website:',
+  store: true,
+}];
+
 test('should storedYoDefaults', (t) =>
-  t.same(storedYoDefaults('unicorns'), 'unicorns'));
+  t.same(storedYoDefaults(input), {
+    moduleVersion: '0.0.0',
+    moduleLicense: 'MIT',
+    moduleTest: 'tape',
+  }));
 
 test('should storedYoDefaults invalid input', (t) =>
-  t.same(storedYoDefaults(), undefined));
+  t.throws(storedYoDefaults(), /q/));
